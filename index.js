421d269ef40a3b363d94dfb652834505e57785dd
Jan Obladen <obladen@datenwelt.net>
2017-05-15 13:49:39 +0200

    Initial commit after factoring out from cargo-auth.

---
+++ index.js
@@ -0,0 +1,24 @@
+const API = require('src/api');
+const Check = require('src/check');
+const Config = require('src/config');
+const Daemon = require('src/daemon');
+const Mailer = require('src/mailer');
+const MQ = require('src/mq');
+const Router = require('src/router');
+const RSA = require('src/rsa');
+const Server = require('src/server');
+
+
+module.export = {
+	
+	API: API,
+	Check: Check,
+	Config: Config,
+	Daemon: Daemon,
+	Mailer: Mailer,
+	MQ: MQ,
+	Router: Router,
+	RSA: RSA,
+	Server: Server
+	
+};
